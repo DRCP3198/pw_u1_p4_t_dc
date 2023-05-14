7d03774742e5dce7bc563d52ffc90680a9cab9ad
Dillan Coloma <renatocoloma2010@gmail.com>
2023-05-14 12:14:28 -0500

    tarea 3 comandos

---
+++ src/main/java/com/example/demo/PwU1P4TDcApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TDcApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TDcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Mi Tarea 3 Dillan Coloma");
+	}
+
+}
